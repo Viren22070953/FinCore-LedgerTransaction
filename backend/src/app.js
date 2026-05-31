913fdd18ec6e638da467b0ec256478768d56b540
Viren22070953 <your-email@example.com>
2026-05-31 17:19:26 +0530

    backend deployement changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const app=express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","http://127.0.0.1:5173", "https://fin-core-ledger-transaction.vercel.app/"],
     credentials: true,
   })
 );
